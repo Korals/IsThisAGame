fd2865fe4cf1be642f2368291a9ade1516a74a46
Karolis Zelvys <koralbish@gmail.com>
2021-09-08 16:26:57 +0300

    Minor tweak to Files.cs

--- MaybeGame/Files.cs
+++ MaybeGame/Files.cs
@@ -1,10 +1,10 @@
-﻿namespace MaybeGame
-{
-    partial class MainProgram
-    {
-        public class Files
-        {
+﻿using System;
+using System.Linq;
 
-        }
+namespace MaybeGame
+{
+    public class Files
+    {
+
     }
 }
